fb8d0ba595244faac8822bf9aa475da984aa18e6
gucci2shoes <guzreister@gmail.com>
2025-11-13 12:17:02 -0800

    Update Day 3 message

--- script.js
+++ script.js
@@ -64,16 +64,11 @@
       unlockTime: '12:00',
       videoUrl: withCache('day3.mp4'),
       videoNote:
-        'For today, I thought you might like to be able to check on Kitty while you&apos;re gone, so I set up the bird buddy inside :). Here&apos;s the first video, and if you want to check in on her live, you can <a href="https://mybirdbuddy.com/my-account/" target="_blank" rel="noreferrer">login to Bird Buddy online</a> or download the Bird Buddy app, just text me and I&apos;ll share the password there so that it isn&apos;t published online. I love you!',
+        'For today, I thought you might like to be able to check on Kitty while you're gone, so I set up the bird buddy inside :). Here's <a href="https://app.mybirdbuddy.com/media/san-diego-united-states/27ed5203-81b1-4ede-b2e8-0cc59039c7b5" target="_blank" rel="noreferrer">the first video</a> (it identified her as a squirrel), and if you want to check in on her live, you can download the Bird Buddy app, just text me and I'll share the password there so that it isn't published online. I love you!',
       quote: {
         text: 'Never give in, never give in, never, never, never, never—in nothing, great or small, large or petty—never give in except to convictions of honor and good sense.',
         attribution: 'Winston Churchill'
       },
-      treat: {
-        type: 'code',
-        payload: 'LUNCH-1313',
-        label: 'Lunch pickup code'
-      }
     },
     {
       id: 4,
